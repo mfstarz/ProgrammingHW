cf802aea51ca89eddf86dbcc57e4acea53c4e046
mfstarz <xtcfakelv@gmail.com>
2022-10-18 10:22:50 +0300

    Fixed HW3_2

--- HW3_2/HW3_2/Source.cpp
+++ HW3_2/HW3_2/Source.cpp
@@ -1,47 +1,56 @@
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 
 using namespace std;
 
-int main() {
-	//1200 1200 1 != 0
-	setlocale(LC_ALL, "Russian");
-	float s, m, n, r, m2;
-	cout << "Введите S: ";
-	cin >> s;
-	if (s <= 0) {
-		cout << "Нельязя взять кредит на <= 0р.";
-		return 0;
-	}
-	cout << "Введите M: ";
-	cin >> m;
-	if (m == 0) {
-		cout << "P = 0";
-		return 0;
-	}
-	cout << "Введите N: ";
-	cin >> n;
-	if (n <= 0) {
-		cout << "Нельзя взять кредит на <= 0 лет.";
-		return 0;
-	}
-	for (float p = 0; p < 100; p++) {
-		r = p / 100;
-		if (p == 0) {
-			m2 = 0;
-		}
-		m2 = (s * r * pow((1 + r), n)) / (12 * ((pow(1 + r, n) - 1)));
-		m = round(m * 1000) / 1000;
-		m2 = round(m2 * 1000) / 1000;
-		if (p == 0) {
-			m2 = m;
-		}
-		//cout << setprecision(10) << m << " | " << m2 << " | " << p << "\n";
-		if (m == m2) {
-			cout << "P = " << p;
-			return 0;
-		}
-	}
-	return 0;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите S:\n";
+    double S;
+    cin >> S;
+    cout << "Введите m:\n";
+    double m;
+    cin >> m;
+    cout << "Введите n:\n";
+    double n;
+    cin >> n;
+    if (!S || !m || !n)
+    {
+        cout << "Ошибка!\n";
+        return 0;
+    }
+    double step = 1;
+    double p = 0;
+    int points_left = 6;
+    int infinity_counter = 0;
+    double prev_result, result, r, one_plus_r, part_one, part_two;
+    result = 0;
+    while (points_left)
+    {
+        p += step;
+        r = (p / 100);
+        one_plus_r = pow((1 + r), n);
+        part_one = S * r * one_plus_r;
+        part_two = 12 * (one_plus_r - 1);
+        prev_result = result;
+        result = part_one / part_two;
+        if (result > m)
+        {
+            result = prev_result;
+            p -= step;
+            step *= pow(10, -1);
+            points_left--;
+        }
+        else if (result == m) {
+            break;
+        }
+        if (infinity_counter++ == 1000)
+        {
+            cout << "Ошибка!";
+            break;
+        }
+    }
+    cout << p << endl;
+    return 0;
 }
