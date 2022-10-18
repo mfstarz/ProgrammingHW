2ccfbf2cc95ccfcfc83224d4b6981a562035ff8b
mfstarz <xtcfakelv@gmail.com>
2022-10-18 10:46:49 +0300

    Fixed HW4_7

--- HW4_7/HW4_7/Source.cpp
+++ HW4_7/HW4_7/Source.cpp
@@ -2,11 +2,34 @@
 
 using namespace std;
 
-float randint() {
+void randint_1(float amount) {
+	amount++;
+	int m = 37;
+	int b = 3;
+	int c = 64;
+	int s = 0;
 
+	while (--amount) {
+		s = (m * s + b) % c;
+		cout << s << "\n";
+	}
+}
+
+void randint_2(float amount) {
+	amount++;
+	int m = 25173;
+	int b = 13849;
+	int c = 65537;
+	int s = 0;
+
+	while (--amount) {
+		s = (m * s + b) % c;
+		cout << s << "\n";
+	}
 }
 
 int main() {
-	cout << randint();
+	randint_1(5);
+	randint_2(5);
 	return 0;
 }
